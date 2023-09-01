179c2a2a8b350d823f14fa5c55ea1b4b1958cada
Nigohmahram <usmonovmaxsudovich@gmail.com>
2023-09-01 21:19:41 +0900

    High order component

---
+++ src/layout/footer/footer.tsx
@@ -0,0 +1,7 @@
+import { FooterProps } from './footer.props';
+
+const Footer = ({ ...props }: FooterProps): JSX.Element => {
+	return <div {...props}>Footer</div>;
+};
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,58 @@
 import { useState } from 'react';
 import { Heading, Text, Tag, Button, Input, TextArea, Rating, Card } from '../components';
-
+import { withLayout } from '../layout/layout';
+import axios from 'axios';
+import { GetServerSideProps } from 'next';
 
 const Index = () => {
-  const [isClick, setIsClick] = useState(false);
-  const [rating, setRating] = useState<number>(4);
-  return (
-    <div>
-      <Heading tag='h1'>Heading</Heading>
-      <Text size='l'>Text</Text>
-      <Tag size='s' color='red'>Legendary</Tag>
-      <Tag size='m' color='green'>Legendary</Tag>
-      <Button appearance='primary'>Primary</Button>
-      <Button appearance='ghost'>Ghost</Button>
-      <Button appearance='ghost' arrow={isClick ? 'down' : 'right'} onClick={() => setIsClick (prev => !prev)}>
+	const [isClick, setIsClick] = useState(false);
+	const [rating, setRating] = useState<number>(4);
+	return (
+		<>
+			<Heading tag='h1'>Heading</Heading>
+			<Text size='l'>Text</Text>
+			<Tag size='s' color='red'>
+				Legendary
+			</Tag>
+			<Tag size='m' color='green'>
+				Legendary
+			</Tag>
+			<Button appearance='primary'>Primary</Button>
+			<Button appearance='ghost'>Ghost</Button>
+			<Button appearance='ghost' arrow={isClick ? 'down' : 'right'} onClick={() => setIsClick(prev => !prev)}>
 				Arrow
 			</Button>
-      <Button appearance='primary' arrow='down'>
+			<Button appearance='primary' arrow='down'>
 				down
 			</Button>
-      <br />
-      <Input placeholder='Enter'/>
-      <TextArea placeholder='Message'/>
+			<br />
+			<Input placeholder='Enter' />
+			<TextArea placeholder='Message' />
 
-      <br/>
-      <Rating rating={rating} isEditabled={true} setRating={setRating}/>
-      <br />
-      <Card color='white'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis neque incidunt suscipit voluptate ratione facere quos amet. Adipisci, cum eaque?
-      </Card>
-       <br />
-       <Card color='primary'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis neque incidunt suscipit voluptate ratione facere quos amet. Adipisci, cum eaque?
-      </Card>
-    </div>
-  );
+			<br />
+			<Rating rating={rating} isEditabled={true} setRating={setRating} />
+			<br />
+			<Card color='white'>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis neque incidunt suscipit voluptate ratione facere quos
+				amet. Adipisci, cum eaque?
+			</Card>
+			<br />
+			<Card color='primary'>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis neque incidunt suscipit voluptate ratione facere quos
+				amet. Adipisci, cum eaque?
+			</Card>
+		</>
+	);
 };
 
-export default Index;
+export default withLayout(Index);
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	const { data } = await axios.post('http://localhost:8100/page-find', { firstCategory: 0 });
+
+	return {
+		props: {
+			data,
+		},
+	};
+};
